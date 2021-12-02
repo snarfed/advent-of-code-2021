b05336731dc79eede66471ef38fd7878f8d405a4
Ryan Barrett <git@ryanb.org>
2021-12-02 06:48:33 -0800

    day 2 part 2

---
+++ day-2-2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Usage: ./day-2-2.sh < day-2-2.input
+
+set -x
+
+declare -i horiz=0 depth=0 aim=0
+while read -r op num; do
+    if [[ $op == 'forward' ]]; then
+        horiz+=num
+        depth+=`expr $aim \* $num`
+    elif [[ $op == 'down' ]]; then
+        aim+=num
+    else
+        aim=aim-num
+    fi
+done
+echo $horiz $depth $aim `expr $horiz \* $depth`
