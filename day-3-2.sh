8a9c73d71f3d8bdd5b273a59f1c9f9700678de5b
Ryan Barrett <git@ryanb.org>
2021-12-04 20:44:33 -0800

    got day 3 part 2 working

--- day-3-2.sh
+++ day-3-2.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+set -x
 
-# Ran this, got 2576 * 1518 == 3910368 as the answer, but adventofcode.com says
-# that's incorrect. Debugged, haven't yet figured out where I went wrong.
+cp day-3.input most
+cp day-3.input least
+oxy=
+co2=
+digit='cut -c $i $which | sort | uniq -c | sort -n $arg | head -n 1 | cut -c9'
+for i in `seq 1 12`; do
+  oxy=${oxy}`arg=-r which=most eval $digit`
+  co2=${co2}`arg=- which=least eval $digit`
 
-rate='seq 1 12 | xargs -n 1 -I %% bash -c "cut -c %% day-3.input | sort | uniq -c | sort -n $arg | head -n 1 | cut -c9" | xargs echo | tr -d " "'
-least=`arg= eval $rate`
-most=`arg=-r eval $rate`
+  cp -f most prev
+  grep -E ^$oxy prev > most
+  cp -f least prev
+  grep -E ^$co2 prev > least
+done
 
-search="xargs -n 1 -I %% bash -c 'grep -E ^\${base::%%} day-3.input' | head -n 1 | sed 's/^/ibase=2;obase=A;/' | bc"
-# Got 101000010000 for oxygen (most common bits) with all digits,
-# 010111101110 for CO2 (least common bits) with 11 out of 12 digits.
-expr `seq 12 -1 1 | base=$most eval $search` \* \
-  `seq 12 -1 1 | base=$least eval $search`
+expr `echo "ibase=2;obase=A;$oxy" | bc` \* `echo "ibase=2;obase=A;$co2" | bc`
