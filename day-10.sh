a5936129d217c4d0fac05c20be1331840d3a9805
Ryan Barrett <git@ryanb.org>
2021-12-10 08:13:59 -0800

    day 10 part 2

--- day-10.sh
+++ day-10.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#
+# PART 1
+#
 cp -f day-10.input cur
 
 while grep -q -E '\(\)|\[]|\{}|<>' cur; do
@@ -11,3 +15,17 @@ expr `grep -c -E '^\)' cur` \* 3 + \
      `grep -c -E '^]' cur` \* 57 + \
      `grep -c -E '^}' cur` \* 1197 + \
      `grep -c -E '^>' cur` \* 25137
+
+
+#
+# PART 2
+#
+cp -f day-10.input cur
+
+while grep -q -E '\(\)|\[]|\{}|<>' cur; do
+    sed -i '' -E 's/\(\)|\[]|\{}|<>//g' cur
+done
+
+grep -E '^[\([{<]+$' cur | sed 's/(/1/g; s/\[/2/g; s/{/3/g; s/</4/g; s/$/0/' | sed -E 's/([0-9])/(\1 + 5 * /g; s/$/0/' | bc | sort -n
+
+# middle score is line 25
