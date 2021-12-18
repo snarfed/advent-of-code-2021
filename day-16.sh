a513a9eac184736562a46d8faba02e18b4ab8b06
Ryan Barrett <git@ryanb.org>
2021-12-18 12:18:04 -0800

    day 16 part 2, almost done

--- day-16.sh
+++ day-16.sh
@@ -7,15 +7,19 @@
 set -e
 # set -x
 
-num=${1:-9999}
+num=${1:-99999}
+skip=0
+
 read data
 
-while [[ ! $data =~ ^0*$ && (( num > 0 )) ]]; do
+while [[ ! $data =~ ^0*$ && (( $num > 0 )) ]]; do
     ver=${data:0:3}
-    echo `echo "ibase=2;obase=A;${ver}" | bc`
+    # echo version `echo "ibase=2;obase=A;${ver}" | bc`
     type=${data:3:3}
     data=${data:6}
+
     if [[ $type == '100' ]]; then
+        # scalar
         bits=''
         while true; do
             bits=${bits}${data:1:4}
@@ -23,23 +27,49 @@ while [[ ! $data =~ ^0*$ && (( num > 0 )) ]]; do
             data=${data:5}
             if [[ $sign == '0' ]]; then break; fi
         done
-        # echo num `echo "ibase=2;obase=A;$bits" | bc`
+        if (( $skip <= 0 )); then
+            echo -n `echo "ibase=2;obase=A;$bits" | bc`,
+        fi
+
     else
+        # operator and subpackets
         sign=${data:0:1}
         data=${data:1}
+
+        case `echo "ibase=2;obase=A;${type}" | bc` in
+            0 ) ops='sum([' ; ope='])' ;;
+            1 ) ops='math.prod([' ; ope='])' ;;
+            2 ) ops='min([' ; ope='])' ;;
+            3 ) ops='max([' ; ope='])' ;;
+            5 ) ops='int(operator.gt(' ; ope='))'  ;;
+            6 ) ops='int(operator.lt(' ; ope='))'  ;;
+            7 ) ops='int(operator.eq(' ; ope='))'
+        esac
+        if [[ $ops == '' ]]; then
+            echo "UNKNOWN TYPE $type"
+            exit 1
+        fi
+        # echo TYPE $type
+
         if [[ $sign == '0' ]]; then
             len=`echo "ibase=2;obase=A;${data:0:15}" | bc`
-            data=${data:15}
             # echo "$len bits of subpackets"
-            echo ${data:0:${len}} | ./day-16.sh
+            data=${data:15}
+            if (( $skip <= 0 )); then
+                python3 -c "import operator, math; print(${ops}`echo ${data:0:${len}} | ./day-16.sh`${ope}, end=',')"
+            fi
             data=${data:${len}}
         else
-            subs=`echo "ibase=2;obase=A;${data:0:11}" | bc`
+            num_subs=`echo "ibase=2;obase=A;${data:0:11}" | bc`
+            # echo "$num_subs subpackets"
             data=${data:11}
-            # echo "$subs subpackets"
-            # echo ${data} | ./day-16.sh $subs
+            if (( $skip <= 0 )); then
+                python3 -c "import operator, math; print(${ops}`echo ${data} | ./day-16.sh $num_subs`${ope}, end=',')"
+            fi
+            skip=$(( $num_subs + 1 ))
             # exit
         fi
     fi
     num=$(( num - 1 ))
+    skip=$(( skip - 1 ))
 done
